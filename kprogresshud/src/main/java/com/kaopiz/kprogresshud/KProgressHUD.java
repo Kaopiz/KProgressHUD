d694c9e0decec2c6dc63c84470794a23670aa44d
zhouu <iamzhouu@gmail.com>
2016-01-16 00:01:58 +0800

    Overload the current create() method and remove some help method

--- kprogresshud/src/main/java/com/kaopiz/kprogresshud/KProgressHUD.java
+++ kprogresshud/src/main/java/com/kaopiz/kprogresshud/KProgressHUD.java
@@ -72,44 +72,15 @@ public class KProgressHUD {
         return new KProgressHUD(context);
     }
 
-    /**
-     * Create a new HUD. Specify the HUD style of {@link Style.SPIN_INDETERMINATE}. For convenient only.
-     *
-     * @param context Activity context that the HUD bound to
-     * @return An unique HUD instance
-     */
-    public static KProgressHUD createForSpin(Context context) {
-        return new KProgressHUD(context).setStyle(Style.SPIN_INDETERMINATE);
-    }
-
-    /**
-     * Create a new HUD. Specify the HUD style of {@link Style.PIE_DETERMINATE}. For convenient only.
-     *
-     * @param context Activity context that the HUD bound to
-     * @return An unique HUD instance
-     */
-    public static KProgressHUD createForPie(Context context) {
-        return new KProgressHUD(context).setStyle(Style.PIE_DETERMINATE);
-    }
-
-    /**
-     * Create a new HUD. Specify the HUD style of {@link Style.BAR_DETERMINATE}. For convenient only.
-     *
-     * @param context Activity context that the HUD bound to
-     * @return An unique HUD instance
-     */
-    public static KProgressHUD createForBar(Context context) {
-        return new KProgressHUD(context).setStyle(Style.BAR_DETERMINATE);
-    }
-
-    /**
-     * Create a new HUD. Specify the HUD style of {@link Style.ANNULAR_DETERMINATE}. For convenient only.
-     *
-     * @param context Activity context that the HUD bound to
-     * @return An unique HUD instance
-     */
-    public static KProgressHUD createForAnnular(Context context) {
-        return new KProgressHUD(context).setStyle(Style.ANNULAR_DETERMINATE);
+  /**
+   * Create a new HUD. specify the HUD style (if you use a custom view, you need {@code KProgressHUD.create(Context context)}).
+   *
+   * @param context Activity context that the HUD bound to
+   * @param style One of the KProgressHUD.Style values
+   * @return An unique HUD instance
+   */
+    public static KProgressHUD create(Context context, Style style) {
+        return new KProgressHUD(context).setStyle(style);
     }
 
     /**
